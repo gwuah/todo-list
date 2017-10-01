64fed359a851476705e3041bef94f18ed2266684
Griffith Awuah <griffith@dolphtech.com>
2017-10-01 22:21:35 +0100

    implemented task deletion feature

--- js/app.js
+++ js/app.js
@@ -1,15 +1,11 @@
 
 /* Helper Functions*/
-const getById = function(selector) {
-    return document.getElementById(selector)
+const getById = function(base, selector) {
+    return base.getElementById(selector)
 }
 
-const getBySelector = function(selector) {
-    return document.querySelector(selector)
-}
-
-const appendTask = function(document, task) {
-    incompleteTasks.innerHTML += task ;
+const appendTask = function(task) {
+    incompleteTasks.appendChild(task) ;
 }
 
 const parseDateTime = function() {
@@ -23,19 +19,21 @@ const parseDateTime = function() {
         minute: dateTime[6]
     }
 }
-
+;
 const createTask = function(name) {
     const dateTime = parseDateTime() ;
-    let task = 
-        `<li data-id="${taskNumber}">
-            <input type="checkbox">
+    let task = document.createElement("li");
+    task.setAttribute("data-id", taskNumber);
+    task.innerHTML = 
+        `<input type="checkbox">
             <label>${name}</label>
             <span>${dateTime.day} ${dateTime.month} ${dateTime.date} ${dateTime.year}</span>
             <span>${dateTime.hour} ${dateTime.minute}</span>
-            <input type="text">
+            <input type="text" class="editBox">
             <button class="edit">Edit</button>
             <button class="delete">Delete</button>
-        </li>` ;
+        ` ;
+    console.log(typeof task)
     return task
     
 }
@@ -44,45 +42,48 @@ const addTask = function() {
     console.log(`adding New Task With id ${taskNumber}`);
     const taskName = taskInput.value;
     const task = createTask(taskName);
-    appendTask(document, task);
+    appendTask(task);
+    taskNumber += 1;
+    bindEventListener(task)
 }
 
 const deleteTask = function(parent, taskId) {
     console.log(`deleting Task ${taskId}`);
-    const trash = getBySelector(`li[data-id="${taskId}"]`);
+    const trash = document.querySelector(`li[data-id="${taskId}"]`);
     trash.parentElement.removeChild(trash); // i was on some high shit
 }
 
-
-
-
-
 /* Handlers */
-const taskInput = getById("newTask");
-const incompleteTaks = getById("incompleteTasks");
-const completedTasks = getById("completedTasks");
-const addNewTask = getById("addNewTask")
-const newTask = getById("newTask")
+const taskInput = getById(document, "newTask");
+const incompleteTaks = getById(document, "incompleteTasks");
+const completedTasks = getById(document, "completedTasks");
+const addNewTask = getById(document, "addNewTask");
+const newTask = getById(document, "newTask");
+
 
 /* Track Number of tasks created */
 let taskNumber = 0;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-document.addEventListener("keydown", (e) => {
+/* Event Listeners */
+document.addEventListener("keydown", e => {
   if (e.keyCode == 13) {
-    // console.log("Enter was pressed")
-    addTask()
+    addTask();
   }
-})
+}); // add task on press on enter key
+
+addNewTask.addEventListener("click", e => {
+    addTask();
+}); // add task on button press
+
+function bindEventListener(task) {
+    const taskComponents = Array.from(task.children);
+    const editBtn = taskComponents.find(el => el.className == "edit");
+    const delBtn = taskComponents.find(el => el.className == "delete");
+
+    delBtn.addEventListener("click", e => {
+        const parentElement = e.target.parentElement;
+        const elementId = e.target.parentElement.getAttribute("data-id");
+        // for every delete button created, bind a function that deletes the respective task
+        deleteTask(parentElement, elementId) 
+    })
+}
